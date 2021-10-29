00585ce1585d9e5272785fe95db05c5329df22cb
Spec7re <dev.andonov@gmail.com>
2021-10-29 15:27:05 +0300

    Update reusable element.

--- js/script.js
+++ js/script.js
@@ -21,23 +21,22 @@ $(document).ready(function(){
       // Add regex to field
       var expression = new RegExp(searchField, "i");
 
+      //Reusable Html Element
+      let htmlElement = (value, key) => {
+	    return  '<li class="list-group-item link-class"><img src="'+value.picture.thumbnail +'" height="40" width="40" class="img-thumbnail" /> '+value.name.first+" "+value.name.last+' | <span class="text-muted">Adress: '+value.location.street+'</span> | <span class="text-muted">Age: '
+            +value.dob.age+'</span><button style="float:right;" data-id='+key+' class="btn btn-info">X</button></li>'
+	}
       // Get JSON Object
       $.getJSON('js/json200.js', function(data) {
         // loop through JSON Object
         $.each(data.results, function(key, value){
-
-          // Reusable Html Element
-          var htmlElement = '<li class="list-group-item link-class"><img src="'+value.picture.thumbnail
-          +'" height="40" width="40" class="img-thumbnail" /> '+value.name.first+" "+value.name.last
-          +' | <span class="text-muted">Adress: '+value.location.street+'</span> | <span class="text-muted">Age: '
-          +value.dob.age+'</span><button style="float:right;" data-id='+key+' class="btn btn-info">X</button></li>'
 
           // RadioButton Name
           if('name' == checkedRadio){
             // Compare by Name
             if (value.name.first.search(expression) != -1 || value.name.last.search(expression) != -1 ){
               // Append content
-              $('#result').append(htmlElement);
+              $('#result').append(htmlElement(value, key));
 
             }
           }
@@ -46,7 +45,7 @@ $(document).ready(function(){
             // Compare by Address
             if (value.location.street.search(expression) != -1){
               // Append content
-              $('#result').append(htmlElement);
+              $('#result').append(htmlElement(value, key));
 
             }
           }
